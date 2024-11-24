1361a88bf8d6a7568ca605c40c31bc00a029b5de
Marcela <marcelavi.sul@gmail.com>
2024-11-24 16:49:27 -0300

    layout

---
+++ app/src/main/java/com/example/urnamacachola2/TelaInicial.java
@@ -0,0 +1,49 @@
+package com.example.urnamacachola2;
+
+import android.os.Bundle;
+import android.view.translation.ViewTranslationRequest;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_inicial);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        /* Trecho de código para deixar em tela cheia */
+        WindowInsetsControllerCompat windowInsetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+        /* Trecho de código para deixar em tela cheia */
+
+        // Botões
+        ImageButton btnConfirmar = findViewById(R.id.btnConfirmar);
+
+        // Ações
+        btnConfirmar.setOnClickListener(view -> {
+            Toast.makeText(TelaInicial.this, "Calma que não tem nada ainda",
+                    Toast.LENGTH_LONG).show();
+        });
+    }
+}
